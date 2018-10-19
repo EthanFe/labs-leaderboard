a7e239186de7f57f0cb358f12ae829fb1d39332b
EthanFe <sneakcow@gmail.com>
2018-10-18 22:02:36 -0500

    we're so dynamic woooo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,16 @@ require 'open-uri'
 
 class UsersController < ApplicationController
   def index
+    
+  end
 
+  def get_labs_completed_for_user(agent, user, base_url)
+    profile_url = base_url + user
+    doc = agent.get(profile_url)
+    labs = doc.css("div.col--size-1of4 > span.heading").text
+  end
+
+  def get_user_data
     email = ENV['LEARN_LOGIN_EMAIL']
     password = ENV['LEARN_LOGIN_PASSWORD']
 
@@ -21,57 +30,9 @@ class UsersController < ApplicationController
     # Submit the form
     page = form.submit form.buttons.first
 
-    users = [
-      "kapham2",
-      "Richardojo86",
-      "nickluong",
-      "gwatson86",
-      "spraguesy",
-      "HeadyT0pper",
-      "V10LET",
-      "mwilliamszoe",
-      "NaebIis",
-      "sparkbold-git",
-      "chelsme",
-      "EthanFe",
-      "morgvanny",
-      "jordantredaniel"
-    ]
-
-    @emoji_list = {
-      "kapham2": "doge",
-      "Richardojo86": "hotpocket",
-      "gwatson": "panda_face",
-      "nickluong": "crying_frog",
-      "chelsme": "lasagnacat",
-      "EthanFe": "monkey_face",
-      "V10LET": "christmas_tree",
-      "HeadyT0pper": "table_tennis_paddle_and_ball",
-      "NaebIis": "cat",
-      "mwilliamszoe": "fries",
-      "sparkbold-git": "metro",
-      "spraguesy": "the_plague",
-      "jordantredaniel": "jordan",
-      "morgvanny": "morgan-freeman",
-    }
-
     base_url = "https://learn.co/"
 
-    @users = []
-    users.each do |user|
-      labs = get_labs_completed_for_user(agent, user, base_url).to_i
-      @users << {name: user, labs_completed: labs}
-    end
-    @users = @users.sort_by do |user|
-      user[:labs_completed]
-    end
-    @max_labs_completed = @users.last[:labs_completed]
-    @users_json = @users.to_json
-  end
-
-  def get_labs_completed_for_user(agent, user, base_url)
-    profile_url = base_url + user
-    doc = agent.get(profile_url)
-    labs = doc.css("div.col--size-1of4 > span.heading").text
+    labs = get_labs_completed_for_user(agent, params[:name], base_url).to_i
+    render json: {name: params[:name], labs: labs}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index]
   root 'users#index'
+
+  get 'users/:name', to: "users#get_user_data"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
